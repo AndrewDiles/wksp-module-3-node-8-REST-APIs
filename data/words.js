1c022d96fc21ac6cf0a6955593dda139f2b9e3dc
Andrew Diles <A_Diles@hotmail.com>
2020-03-04 23:42:48 -0500

    Don't Fred, bears want to cuddle

---
+++ data/words.js
@@ -0,0 +1,24 @@
+const words = [
+{ 'word': 'abruptly',   'id': '0',  'letterCount': '8' },
+{ 'word': 'blizzard',   'id': '1',  'letterCount': '8' },
+{ 'word': 'crypt',      'id': '2',  'letterCount': '5' },
+{ 'word': 'equip',      'id': '3',  'letterCount': '5' },
+{ 'word': 'frizzled',   'id': '4',  'letterCount': '8' },
+{ 'word': 'glyph',      'id': '5',  'letterCount': '5' },
+{ 'word': 'ivy',        'id': '6',  'letterCount': '3' },
+{ 'word': 'jogging',    'id': '7',  'letterCount': '7' },
+{ 'word': 'klutz',      'id': '8',  'letterCount': '5' },
+{ 'word': 'onyx',       'id': '9',  'letterCount': '4' },
+{ 'word': 'pneumonia',  'id': '10', 'letterCount': '9' },
+{ 'word': 'quizzes',    'id': '11', 'letterCount': '7' },
+{ 'word': 'snazzy',     'id': '12', 'letterCount': '6' },
+{ 'word': 'swivel',     'id': '13', 'letterCount': '6' },
+{ 'word': 'twelfth',    'id': '14', 'letterCount': '7' },
+{ 'word': 'voodoo',     'id': '15', 'letterCount': '6' },
+{ 'word': 'woozy',      'id': '16', 'letterCount': '5' },
+{ 'word': 'zephyr',     'id': '17', 'letterCount': '6' },
+{ 'word': 'zombie',     'id': '18', 'letterCount': '6' },
+{ 'word': 'zealously',  'id': '19', 'letterCount': '9' }
+]
+
+module.exports = words;
--- server.js
+++ server.js
@@ -4,7 +4,60 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+const words = require('./data/words.js');
+
 const PORT = process.env.PORT || 8000;
+
+const handleGuess = (req, res) => {
+    // console.log(req.params.id);
+    // console.log(req.params.letter);
+    let code = [];
+    let letterInWord = false;
+    for (let i=0; i<words[req.params.id].word.length; i++) {
+        if (words[req.params.id].word[i]===req.params.letter) {
+            code.push('T');
+            letterInWord = true;
+        }
+        else code.push('F');
+    }
+    // console.log(code);
+    if (!letterInWord) code = [];
+    // console.log(code);
+    res.send({
+        status: '200',
+        guess: req.params.letter,
+        id: req.params.id,
+        answer: code
+    });
+}
+
+// const handleGameLaunch = (req, res) => {
+//     console.log(req.params);
+//     let wordNum = req.params.id;
+//     console.log('is this working?');
+//     console.log(wordNum);
+//     console.log(words[wordNum]);
+//     res.render('../hangman/hangman', {
+//         'id': wordNum,
+//         'letterCount': words[wordNum].letterCount
+//     });
+// }
+
+const handleGeneration = (req, res) => {
+    let generatedWord = words[Math.floor(1+Math.random()*19)];
+    console.log(generatedWord);
+    console.log(generatedWord.word);
+    console.log(generatedWord.id);
+    console.log(generatedWord.letterCount);
+    res.send({
+        id: generatedWord.id,
+        letterCount: generatedWord.letterCount
+    });
+}
+
+// const handleAccessSpecifiedWordId = (req, res) => {
+//     res.redirect('hangman/index', word = req.params.id);
+// }
 
 express()
     .use(function(req, res, next) {
@@ -19,4 +72,32 @@ express()
 
     // endpoints
 
-    .listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    
+    // .get('/hangman/:id', handleGameLaunch)
+    // .get('/getAWord', handleGeneration)
+    .get('/hangman/getAWord', handleGeneration)
+    .get('/hangman/:id/:letter', handleGuess)
+    // .get('/hangman/:id', handleAccessSpecifiedWordId)  This was for the challenge, I decided to take a break lol
+
+    // .get('/hangman/getAWord', (req, res) => res.send('Testy tesy'))
+    .get('/', (req, res) => res.render('/index'))
+    .get('*', (req, res) => res.send(`404 - You don't know Jack`))
+    .listen(PORT, () => console.log(`Listening on port ${PORT}`))
+
+
+
+    // and use querry by filling in message ewhen hyou clikc on the buttons
+
+
+    // .get(`hangman/guess/:wordID/:letter`, (req,res) => {
+    //     const {params, query} = req;
+    //     console.log(params,query);
+    //     res.send('tofu');
+    // })
+    // or use the querry by adding a message
+    // .get(`hangman/guess/:wordID/`, (req,res) => {
+    //     const {params, query} = req;
+    //     console.log(params,query);
+    //     res.status(300).send('tofu');
+    // })
+
